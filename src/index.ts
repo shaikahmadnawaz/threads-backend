ecf4abf7dafadcf08811a080f70024294a37da25
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-07-24 17:58:42 +0530

    server working

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
 async function init() {
@@ -7,11 +8,24 @@ async function init() {
 
   app.use(express.json());
 
+  const server = new ApolloServer({
+    typeDefs: `
+    type Query {
+      hello: String!
+    }
+    `,
+    resolvers: {
+      Query: { hello: () => "Hello World" },
+    },
+  });
+
+  await server.start();
+
   app.get("/", (req, res) => {
     res.json({ message: "Server is up and running" });
   });
 
-  app.use("/graphql", expressMiddleware());
+  app.use("/graphql", expressMiddleware(server));
 
   app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
